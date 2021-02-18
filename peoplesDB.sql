db23adda4a9c6bc8fa0d5f3b012ddc2c3281f23c
Luiz Felipe Rosa da Silveira <luizfelipers19@gmail.com>
2021-02-18 16:26:12 -0300

    Update peoplesDB.sql

--- peoplesDB.sql
+++ peoplesDB.sql
@@ -29,3 +29,59 @@ CREATE TABLE pets(
     owner_id INT -- represents the Foreign Key to the People's table
 
 );
+SHOW TABLES;
+
+DESCRIBE addresses;
+
+-- changes a column in a way that it's not the primary key anymore
+ALTER TABLE addresses
+DROP PRIMARY KEY,
+CHANGE id id INT;
+
+ALTER TABLE addresses ADD PRIMARY KEY (id);
+
+DESCRIBE people;
+
+ALTER TABLE people ADD PRIMARY KEY (id);
+ALTER TABLE addresses ADD PRIMARY KEY (id);
+
+DESCRIBE addresses;
+DESCRIBE people;
+
+ALTER TABLE people ADD CONSTRAINT FK_PeopleAddress FOREIGN KEY (address_id) REFERENCES addresses(id);
+
+ALTER TABLE people DROP FOREIGN KEY FK_PeopleAddress;
+
+SHOW DATABASES;
+USE test;
+SHOW TABLES;
+DESCRIBE people;
+
+SELECT * FROM pets;
+ALTER TABLE pets ADD CONSTRAINT u_species UNIQUE (species);
+
+DESCRIBE pets;
+ALTER TABLE pets DROP INDEX u_species;
+
+SELECT * FROM pets;
+ALTER TABLE pets CHANGE `species` `animal_type`VARCHAR(20);
+
+ALTER TABLE pets CHANGE `animal_type` `species`VARCHAR(20);
+DESCRIBE pets;
+DESCRIBE addresses;
+ALTER TABLE addresses MODIFY city VARCHAR(20);
+DESCRIBE addresses;
+ALTER TABLE addresses MODIFY city VARCHAR(30);
+
+SHOW TABLES;
+DESCRIBE people;
+ALTER TABLE people ADD PRIMARY KEY (id);
+DESCRIBE pets;
+
+-- Exercise 1 - Changing Keys, columns, constraints and datatypes
+ALTER TABLE pets ADD CONSTRAINT FK_OwnerID FOREIGN KEY (owner_id) REFERENCES people(id);
+ALTER TABLE people ADD COLUMN email VARCHAR(50);
+ALTER TABLE people ADD CONSTRAINT u_email UNIQUE (email);
+ALTER TABLE pets CHANGE `name` `first_name` VARCHAR(20);
+DESCRIBE addresses;
+ALTER TABLE addresses MODIFY zip_code CHAR(7);
